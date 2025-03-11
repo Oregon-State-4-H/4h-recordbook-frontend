b8362a23b910f652a72388276b3911446dad3a6e
Joy <146790408+joy-totheworld@users.noreply.github.com>
2025-03-10 23:18:19 -0700

    Loading Component

---
+++ my-app/src/components/CloverLoader.tsx
@@ -0,0 +1,32 @@
+import CircularProgress, {
+  CircularProgressProps,
+} from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+
+declare module "*.png";
+
+export default function CloverLoader() {
+  return (
+    <Box sx={{ position: "relative", display: "inline-flex", top: "40px" }}>
+      <CircularProgress size="140px" thickness={2} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          style={{ width: "60%", height: "60%" }}
+          src="/photos/400x400.png"
+        />
+      </Box>
+    </Box>
+  );
+}
--- my-app/src/components/Landing/ContactCardsY1.tsx
+++ my-app/src/components/Landing/ContactCardsY1.tsx
@@ -1,13 +1,9 @@
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Container from "@mui/material/Container";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
 
 function ContactCards() {
   return (
