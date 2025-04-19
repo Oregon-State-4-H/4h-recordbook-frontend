db06664fd77fe0b8b3ec610625b7851c0c8bc9c6
Joy <146790408+joy-totheworld@users.noreply.github.com>
2025-04-19 14:24:50 -0700

    Update page.tsx

--- src/app/dummySignIn/page.tsx
+++ src/app/dummySignIn/page.tsx
@@ -20,6 +20,7 @@ export default function DummySignIn() {
       hrefTitle: "/",
       mobileTopIcon: "none",
       NavbarLinks: [],
+      Bookmarks: [],
     };
 
     updateFunction(navbarContextPageValues);
