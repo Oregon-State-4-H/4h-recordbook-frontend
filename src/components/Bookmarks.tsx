1004ec40c7a5c52538b630db98785610568ffc4c
Joy <146790408+joy-totheworld@users.noreply.github.com>
2025-04-19 13:40:50 -0700

    Update Bookmarks.tsx

--- src/components/Bookmarks.tsx
+++ src/components/Bookmarks.tsx
@@ -18,105 +18,112 @@ import IconButton from "@mui/material/IconButton";
 interface DashboardBookmarksProps {
   setDashboardBookmarks: (newBookmarks: Bookmark[]) => void;
   bookmarks: Bookmark[];
+  jwt: string;
 }
 
-// export function BookmarkButton() {
-//   const params = useParams();
-//   const [bookmark, setBookmark] = useState<Bookmark | undefined>();
-//   const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
-//   const [isBookmarked, setIsBookmarked] = useState(false);
-//   var pathname = "";
-//   const searchParams = useSearchParams();
+interface BookmarksProps {
+  jwt: string;
+}
 
-//   useEffect(() => {
-//     pathname = router.asPath.slice(1); //get rid of '/' at the beginning since it messes with the api
-//     const checkIfBookmarked = async () => {
-//       try {
-//         const currentUrl = `${pathname}${searchParams.toString()}`;
-//         const bookmarks: Bookmark[] = await fetchAllBookmarks();
-//         setBookmarks(bookmarks);
-//         console.log("bookmarks: ", bookmarks);
-//         console.log("currentUrl: ", currentUrl);
-//         setBookmark(bookmarks.find((b) => b.link === currentUrl));
-//         setIsBookmarked(
-//           bookmarks.find((b) => b.link === currentUrl) ? true : false
-//         );
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     checkIfBookmarked();
-//   }, [pathname, searchParams, router.isReady]);
+export function BookmarkButton({ jwt }: BookmarksProps) {
+  const params = useParams();
+  const [bookmark, setBookmark] = useState<Bookmark | undefined>();
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  var pathname = "";
+  const searchParams = useSearchParams();
 
-//   const handleBookmarkToggle = async () => {
-//     pathname = router.asPath.slice(1); //get rid of '/' at the beginning since it messes with the api
-//     const currentUrl = `${pathname}${searchParams.toString()}`;
-//     if (isBookmarked) {
-//       if (!confirm("Are you sure you want to remove this bookmark?")) {
-//         return;
-//       }
-//       if (typeof bookmark != "undefined") {
-//         try {
-//           const deleteSucceeded = await deleteBookmark(bookmark.id);
-//           if (deleteSucceeded) {
-//             setBookmarks((prevBookmarks) =>
-//               prevBookmarks.filter(
-//                 (curr_bookmark) => curr_bookmark.id !== bookmark.id
-//               )
-//             );
-//             setIsBookmarked(false);
-//           }
-//         } catch (error) {
-//           console.error(error);
-//         }
-//       }
-//     } else {
-//       const label = prompt("Enter bookmark label:");
-//       if (label == null) {
-//         return;
-//       }
-//       const input: CustomBookmarkFields = {
-//         link: currentUrl,
-//         label: label,
-//       };
-//       try {
-//         const newBookmark = await postBookmark(input);
-//         setBookmarks([...bookmarks, newBookmark]);
-//         setIsBookmarked(true);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     }
-//   };
+  // useEffect(() => {
+  //   pathname = router.asPath.slice(1); //get rid of '/' at the beginning since it messes with the api
+  //   const checkIfBookmarked = async () => {
+  //     try {
+  //       const currentUrl = `${pathname}${searchParams.toString()}`;
+  //       const bookmarks: Bookmark[] = await fetchAllBookmarks();
+  //       setBookmarks(bookmarks);
+  //       console.log("bookmarks: ", bookmarks);
+  //       console.log("currentUrl: ", currentUrl);
+  //       setBookmark(bookmarks.find((b) => b.link === currentUrl));
+  //       setIsBookmarked(
+  //         bookmarks.find((b) => b.link === currentUrl) ? true : false
+  //       );
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   checkIfBookmarked();
+  // }, [pathname, searchParams, router.isReady]);
 
-//   return (
-//     <IconButton aria-label="bookmark" onClick={handleBookmarkToggle}>
-//       {isBookmarked ? (
-//         <MdBookmark id="bookmark" />
-//       ) : (
-//         <MdBookmarkBorder id="bookmark" />
-//       )}
-//     </IconButton>
-//   );
-// }
+  const handleBookmarkToggle = async () => {
+    // pathname = router.asPath.slice(1); //get rid of '/' at the beginning since it messes with the api
+    pathname = "";
+    const currentUrl = `${pathname}${searchParams.toString()}`;
+    if (isBookmarked) {
+      if (!confirm("Are you sure you want to remove this bookmark?")) {
+        return;
+      }
+      if (typeof bookmark != "undefined") {
+        try {
+          const deleteSucceeded = await deleteBookmark(jwt, bookmark.id);
+          if (deleteSucceeded) {
+            setBookmarks((prevBookmarks) =>
+              prevBookmarks.filter(
+                (curr_bookmark) => curr_bookmark.id !== bookmark.id
+              )
+            );
+            setIsBookmarked(false);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    } else {
+      const label = prompt("Enter bookmark label:");
+      if (label == null) {
+        return;
+      }
+      const input: CustomBookmarkFields = {
+        link: currentUrl,
+        label: label,
+      };
+      try {
+        const newBookmark = await postBookmark(jwt, input);
+        setBookmarks([...bookmarks, newBookmark]);
+        setIsBookmarked(true);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
-// export function BookmarkHeader() {
-//   return (
-//     <Box
-//       sx={{
-//         width: "90%",
-//         marginLeft: "5%",
-//         marginRight: "5%",
-//         display: "flex",
-//         flexDirection: "row-reverse",
-//       }}
-//     >
-//       <BookmarkButton />
-//     </Box>
-//   );
-// }
+  return (
+    <IconButton aria-label="bookmark" onClick={handleBookmarkToggle}>
+      {isBookmarked ? (
+        <MdBookmark id="bookmark" />
+      ) : (
+        <MdBookmarkBorder id="bookmark" />
+      )}
+    </IconButton>
+  );
+}
+
+export function BookmarkHeader({ jwt }: BookmarksProps) {
+  return (
+    <Box
+      sx={{
+        width: "90%",
+        marginLeft: "5%",
+        marginRight: "5%",
+        display: "flex",
+        flexDirection: "row-reverse",
+      }}
+    >
+      <BookmarkButton jwt={jwt} />
+    </Box>
+  );
+}
 
 export function DashboardBookmarkButton({
+  jwt,
   bookmarks,
   setDashboardBookmarks,
 }: DashboardBookmarksProps) {
@@ -145,13 +152,13 @@ export function DashboardBookmarkButton({
       }
       let bookmarkID: string = "";
       try {
-        const bookmark = await fetchBookmark(encodedUrl);
+        const bookmark = await fetchBookmark(jwt, encodedUrl);
         bookmarkID = bookmark.id;
       } catch (error) {
         console.error(error);
       }
       try {
-        const deleteSucceeded = await deleteBookmark(bookmarkID);
+        const deleteSucceeded = await deleteBookmark(jwt, bookmarkID);
         if (deleteSucceeded) {
           setDashboardBookmarks(
             bookmarks.filter((bookmark) => bookmark.id !== bookmarkID)
@@ -171,7 +178,7 @@ export function DashboardBookmarkButton({
         label: label,
       };
       try {
-        const newBookmark = await postBookmark(input);
+        const newBookmark = await postBookmark(jwt, input);
         setDashboardBookmarks([...bookmarks, newBookmark]);
         setIsBookmarked(true);
       } catch (error) {
@@ -193,6 +200,7 @@ export function DashboardBookmarkButton({
 
 export function DashboardBookmarkHeader({
   bookmarks,
+  jwt,
   setDashboardBookmarks,
 }: DashboardBookmarksProps) {
   return (
@@ -207,6 +215,7 @@ export function DashboardBookmarkHeader({
     >
       <DashboardBookmarkButton
         bookmarks={bookmarks}
+        jwt={jwt}
         setDashboardBookmarks={setDashboardBookmarks}
       />
     </Box>
