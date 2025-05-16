447bbc21157b4faf117086326ae2f9829a4b55c2
Joy <146790408+joy-totheworld@users.noreply.github.com>
2025-05-16 13:57:39 -0700

    Expense Page

--- src/app/Dashboard/Projects/animal/[data]/Expense/page.tsx
+++ src/app/Dashboard/Projects/animal/[data]/Expense/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import { getAccessToken } from "@auth0/nextjs-auth0";
 import { useParams, usePathname } from "next/navigation";
 import Box from "@mui/material/Box";
@@ -8,6 +9,14 @@ import TitleOnly from "@/components/Projects/TitleOnly";
 import { Project, isProject, fetchProject } from "@/API/ProjectAPI";
 import SubpageCard from "@/components/Projects/SubpageCard";
 import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Modal from "@mui/material/Modal";
 import {
   useNavbar,
   NavbarValues,
@@ -15,75 +24,97 @@ import {
 } from "@/context/NavbarContext";
 import { useBookmark } from "@/context/BookmarkContext";
 import { useProject } from "@/context/ProjectContext";
+import ProjectTableRow from "@/components/Projects/TableRow";
+import CreateButton from "@/components/Projects/CreateIconButton";
+import DynamicPopUp from "@/components/Projects/DynamicPopUp";
+import { EndpointByDynamicPathSuffix } from "@/API/ProjectAPI";
+import subpageOutline from "@/components/Projects/SubpageOutline.json";
+import {
+  Expense,
+  AnimalProjectTypes,
+  fetchSubpageEntriesByProject,
+  emptyExpense,
+} from "@/API/ProjectAPI";
 
 function Dashboard() {
-  const { currNavbarValues, updateFunction } = useNavbar();
+  const { updateFunction } = useNavbar();
   const { updateBookmarks } = useBookmark();
   const params = useParams<{ tag: string; item: string }>();
   const { data }: any = params;
   const [validId, setValidId] = useState(true);
-  const [projectLoaded, setProjectLoaded] = useState(false);
-  let [currProject, setProject] = useState<Project>();
   const [accessToken, setAccessToken] = useState("");
   const { updateProjects, currProjectValues, populated } = useProject();
   const pathname = usePathname();
+  // states for all subpage entries
+  let [allSubpageEntries, setAllSubpageEntries] = useState<Expense[]>([]);
+  const [validSubpage, setValidSubpage] = useState(true);
+  const [subpageDataLoaded, setSubpageDataLoaded] = useState(false);
+
+  // states for how entry data should be displayed
+  const [displayLinks, setDisplayLinks] = useState(false);
+  const [displayDetail, setDisplayDetail] = useState(false);
+  const displayAsLinks = () => {
+    setDisplayLinks(true);
+    setDisplayDetail(false);
+  };
+  const displayAsDetail = () => {
+    setDisplayLinks(false);
+    setDisplayDetail(true);
+  };
+
+  // state for multipurpose input modal
+  const [inputModal, setinputModal] = React.useState(false);
+  let [inputModalEntry, setinputModalEntry] =
+    useState<AnimalProjectTypes>(emptyExpense);
+  let [inputModalPurpose, setinputModalPurpose] = useState<string>("");
+  const handleinputModalClose = () => {
+    setinputModal(false);
+    setinputModalEntry(emptyExpense);
+    setinputModalPurpose("");
+  };
+  const handleinputModalOpen = (
+    currinputModalEntry: AnimalProjectTypes,
+    purpose: string
+  ) => {
+    setinputModal(true);
+    setinputModalEntry(currinputModalEntry);
+    setinputModalPurpose(purpose);
+  };
+
+  // state for multipurpose snackbar alert
+  const [alert, setAlert] = React.useState(false);
+  let [alertText, setAlertText] = useState<String>("");
+  const handleAlertClose = () => {
+    setAlert(false);
+    setAlertText("");
+  };
+  const handleAlertOpen = (currAlertText: string) => {
+    setAlert(true);
+    setAlertText(currAlertText);
+  };
 
   useEffect(() => {
     const getData = async () => {
       try {
         // if the array of all projects is not populated, get project from backend
-        if (!populated) {
-          if (accessToken == "") {
-            const token = await getAccessToken();
-            setAccessToken(token);
-            const projectData = await fetchProject(token, data);
-            if (typeof projectData == "string") {
-              setValidId(false);
-            } else {
-              setProject(projectData);
-              setProjectLoaded(true);
-            }
-          } else {
-            const projectData = await fetchProject(accessToken, data);
-            if (typeof projectData == "string") {
-              setValidId(false);
-            } else {
-              setProject(projectData);
-              setProjectLoaded(true);
-            }
-          }
-        }
-        // else the array of all projects is populated, search for project in array
-        else {
-          var foundProject = currProjectValues.find(
-            (element) => element.id == data
+        if (accessToken == "") {
+          const token = await getAccessToken();
+          setAccessToken(token);
+          const subpageData = await fetchSubpageEntriesByProject<Expense>(
+            token,
+            "expense",
+            data
           );
-          if (typeof foundProject != "undefined") {
-            setProject(foundProject);
-            setProjectLoaded(true);
-          }
-          // if project not found in array, double check backend
-          else {
-            if (accessToken == "") {
-              const token = await getAccessToken();
-              setAccessToken(token);
-              const projectData = await fetchProject(token, data);
-              if (typeof projectData == "string") {
-                setValidId(false);
-              } else {
-                setProject(projectData);
-                setProjectLoaded(true);
-              }
-            } else {
-              const projectData = await fetchProject(accessToken, data);
-              if (typeof projectData == "string") {
-                setValidId(false);
-              } else {
-                setProject(projectData);
-                setProjectLoaded(true);
-              }
-            }
-          }
+          setAllSubpageEntries(subpageData);
+          setSubpageDataLoaded(true);
+        } else {
+          const subpageData = await fetchSubpageEntriesByProject<Expense>(
+            accessToken,
+            "expense",
+            data
+          );
+          setAllSubpageEntries(subpageData);
+          setSubpageDataLoaded(true);
         }
       } catch (error) {
         console.log(error);
@@ -94,8 +125,8 @@ function Dashboard() {
 
   useEffect(() => {
     var navbarContextPageValues: NavbarValues = {
-      mobileTitle: "Project Overview",
-      desktopTitle: "Project Overview",
+      mobileTitle: "Project Expense",
+      desktopTitle: "Project Expense",
       hrefTitle: "/Dashboard",
       mobileTopIcon: "none",
       NavbarLinks: navbarAppLinks,
@@ -110,81 +141,92 @@ function Dashboard() {
   if (!validId) {
     console.log("Invalid project id else statement");
     return <TitleOnly title="Project Not Found" cloverLoader={false} />;
-  } else if (validId && !projectLoaded) {
-    console.log("Loading project else statement");
-    return <TitleOnly title="Loading..." cloverLoader={true} />;
-  } else if (validId && projectLoaded && typeof currProject == "undefined") {
-    console.log("undefined project else statement");
-    return <TitleOnly title="Error (Joy to fix)" cloverLoader={false} />;
-  } else if (projectLoaded && validId && isProject(currProject)) {
-    console.log("Valid project else statement");
-    console.log(currProject);
-    var Subpages: string[] = [];
-    switch (currProject.type) {
-      case "animal":
-        Subpages = [
-          "Animals",
-          "Feed Record",
-          "Feed Inventory",
-          "Expense",
-          "Supplies",
-        ];
-        break;
-      default:
-        break;
-    }
-
+    // } else if (validId && !projectLoaded) {
+    //   console.log("Loading project else statement");
+    //   return <TitleOnly title="Loading..." cloverLoader={true} />;
+    // } else if (validId && projectLoaded && typeof currProject == "undefined") {
+    //   console.log("undefined project else statement");
+    //   return <TitleOnly title="Error (Joy to fix)" cloverLoader={false} />;
+    // } else if (projectLoaded && validId && isProject(currProject)) {
+  } else if (validId) {
     return (
       <Box className="App">
-        {/* For every subpage, generate a clickable card */}
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: { xs: "flex", md: "none" },
-            width: "100%",
-            flexDirection: "column",
-            paddingBottom: "50px",
-          }}
-        >
-          {Subpages &&
-            Subpages.length > 0 &&
-            Subpages.map((item, index) => (
-              <SubpageCard
-                key={index}
-                label={item}
-                path={pathname + "/" + item.replaceAll(" ", "")}
-              />
-            ))}
-        </Box>
         <Box
           sx={{
             width: "90%",
-            display: { xs: "none", md: "block" },
+            marginLeft: "5%",
+            marginRight: "5%",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          {/* first box is placeholder for component that will have preview and download resume pdf */}
+          <Box
+            sx={{
+              width: "50%",
+              margin: "0px",
+              padding: "0px",
+            }}
+          ></Box>
+          <CreateButton handleOpen={handleinputModalOpen} />
+        </Box>
+        <Paper
+          sx={{
+            width: "90%",
+            overflow: "hidden",
             marginLeft: "5%",
             marginRight: "5%",
             marginTop: "15px",
           }}
         >
-          <Grid
-            container
-            rowSpacing={1}
-            columnSpacing={0}
-            sx={{
-              width: "100%",
-            }}
-          >
-            {Subpages &&
-              Subpages.length > 0 &&
-              Subpages.map((item, index) => (
-                <Grid size={6} key={index}>
-                  <SubpageCard
-                    label={item}
-                    path={pathname + "/" + item.replaceAll(" ", "")}
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                {/* in the first row of the table, make a cell for each collumn, holding the label */}
+                <TableRow>
+                  {subpageOutline.expense.headers.map((item, index) => (
+                    <TableCell
+                      key={index}
+                      align="right"
+                      sx={{
+                        textAlign: "center",
+                        minWidth: 0.5 / allSubpageEntries.length,
+                      }}
+                    >
+                      {item.charAt(0).toUpperCase() + item.slice(1)}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {allSubpageEntries.map((item, index) => (
+                  <ProjectTableRow
+                    index={index}
+                    projectEntry={item}
+                    subpage="Expense"
+                    handleOpen={handleinputModalOpen}
                   />
-                </Grid>
-              ))}
-          </Grid>
-        </Box>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+        <Modal
+          open={inputModal}
+          onClose={handleinputModalClose}
+          aria-labelledby="input-modal-title"
+          aria-describedby="input-modal-description"
+        >
+          <DynamicPopUp
+            subpage="Expense"
+            priorEntries={allSubpageEntries}
+            subpageEntry={inputModalEntry}
+            handleModalClose={handleinputModalClose}
+            purpose={inputModalPurpose}
+            handleOpen={handleinputModalOpen}
+            project_id={data}
+          />
+        </Modal>
       </Box>
     );
   }
